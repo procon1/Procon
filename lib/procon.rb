28a74fa9f0c46472eccf182ea4e45c7b2a20ce9f
Akiya Togo <akiya.togo@unisys.co.jp>
2012-09-24 20:10:42 +0900

    procon.rbファイルを修正

--- lib/procon.rb
+++ lib/procon.rb
@@ -1,10 +1,8 @@
 require 'robot'
 
 class Procon
-  def hoge
-    robots = create_robots
-    winner_robots = fight(robots)
-  end
+
+FILES = "/home/atogo1/work/Procon/spec/file2/*"
 
   def create_robots
     robots = []
@@ -22,7 +20,7 @@ class Procon
   # TODO 要修正
   # 読み込むfileの格納フォルダのパス
   def siyousyo
-    specifications = Dir.glob("/home/awatanabe21/Procon1/spec/file2/*")
+    specifications = Dir.glob("#{Procon::FILES}")
     file_count = specifications.count
     siyousyo = []
     for i in 1..file_count
@@ -36,28 +34,5 @@ class Procon
 
 end
 
-def damage(atk, deff)
-  if deff >= atk
-    [atk - deff + 500, 1].max
-  else
-    3 * (atk - deff) + 500
-  end
-end
+p Procon.new.create_robots
 
-def calc_attack(atk, weight)
-  atk - weight ** 2
-end
-
-def calc_deffence(deff, weight)
-  deff + 10 * weight
-end
-
-def calc_endurance(endurance)
-  endurance.min
-end
-
-#
-#p damage(a_atk, b_deff)
-#p calc_attack(a_atk, b_deff)
-#p calc_deffence(b_deff, 10)
-#p calc_endurance([10, 20, 10, 30])
